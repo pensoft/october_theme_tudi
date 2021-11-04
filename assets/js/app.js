bd17ef4cb11475e9bf3ded43bd218b63227019b7
root <root@project-dev.pensoft.local>
2021-11-04 11:59:52 +0200

    open news images in modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -70,6 +70,46 @@ $(document).ready(function() {
 
 	});
 
+
+	$('.content-wrapper.news .content img').each(function(){
+		$(this).attr('id', 'myImg');
+		$(this).addClass('myImages');
+	});
+
+	$('.content-wrapper.news').after('<div id=\"myModal\" class=\"modal\">\n' +
+		'  <span class=\"close_modal\">&times;</span>\n' +
+		'  <img class=\"modal-content\" id=\"img01\">\n' +
+		'  <div id=\"caption\"></div>\n' +
+		'</div>');
+
+
+	// create references to the modal...
+	var modal = document.getElementById('myModal');
+// to all images -- note I'm using a class!
+	var images = document.getElementsByClassName('myImages');
+// the image in the modal
+	var modalImg = document.getElementById("img01");
+// and the caption in the modal
+	var captionText = document.getElementById("caption");
+
+// Go through all of the images with our custom class
+	for (var i = 0; i < images.length; i++) {
+		var img = images[i];
+		// and attach our click listener for this image.
+		img.onclick = function(evt) {
+			modal.style.display = "block";
+			modalImg.src = this.src;
+			captionText.innerHTML = this.alt;
+		}
+	}
+
+	var span = document.getElementsByClassName("close_modal")[0];
+
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+
 });
 
 
